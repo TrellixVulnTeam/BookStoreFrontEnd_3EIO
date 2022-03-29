e622cc8c23c184da89244df76f3e5f033f070d82
Anhdao153 <ndthien.1532000@gmail.com>
2022-03-29 18:35:01 +0700

    git
    asdhfoj

--- src/app/component/order/order.component.ts
+++ src/app/component/order/order.component.ts
@@ -1,4 +1,6 @@
+import { BookServiceService } from 'src/app/service/bookService/book-service.service';
 import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/model/book';
 
 @Component({
   selector: 'app-order',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
-
-  constructor() { }
+  id: string = '';
+  product: Book | undefined;
+  img: string = '';
+  quantity: number = 0;
+  name: string = '';
+  price: number = 0;
+  author: string = '';
+  publication: string = '';
+  publishing_company: string = '';
+  translator: string='';
+  message='';
+  constructor(private bookService: BookServiceService) { }
 
   ngOnInit(): void {
   }
+
+  getProduct() {
+    this.bookService.getDetail(this.id).subscribe(data => {
+      this.product = data;
+      if (this.product != null) {
+        this.img = data.url;
+        this.quantity = data.quantity;
+        this.name = data.name;
+        this.price = data.price;
+        this.id = data.id;
+        this.author = data.author;
+        this.publication = data.publication;
+        this.publishing_company=data.publishing_company;
+        this.translator=data.translator;
+        this.message="Run Well";
+      } else{
+        this.message="Bug";
+      }
+    })
+  }
+  
+
+
+
+
+
 
 }
